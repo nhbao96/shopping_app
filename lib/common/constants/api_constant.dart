59aad0acbeccbf3b96592cc51979b61fb7674b47
BaoNH19 <baonh19@fpt.com.vn>
2022-11-30 16:56:50 +0700

    add key api

--- lib/common/constants/api_constant.dart
+++ lib/common/constants/api_constant.dart
@@ -1,9 +1,10 @@
 class ApiConstant {
-  static final String BASE_URL = "https://serverappsale.herokuapp.com/";
+  static final String BASE_URL = "https://stag-api-public.shoppingtv.vn/api/v1.1/";
   static final String SIGN_IN = "/user/sign-in";
   static final String SIGN_UP = "/user/sign-in";
   static final String PRODUCT = "/product";
   static final String CART = "/cart";
   static final String ADD_CART = "/cart/add";
   static final String UPDATE_CART = "/cart/update";
+  static final String ROUTE_HOME_V2 = "home_v2";
 }
--- lib/common/constants/variable_constant.dart
+++ lib/common/constants/variable_constant.dart
@@ -4,5 +4,11 @@ class VariableConstant {
   static final String ROUTE_SIGNUP =  "sign-up";
   static final String ROUTE_HOME =  "home-page";
   static final String ROUTE_CARTPAGE =  "cart-page";
-
+  static final String CUSTOMER_ID = "0x113dd";
+  static final String API_KEY = "x-api-key";
+  static final String API_KEY_VALUE = "Ns7ZmIeAqDaUm0sA";
+  static final String OTT_KEY = "ott";
+  static final String OTT_KEY_VALUE = "FPTIPTV";
+  static final String X_DEVICE_KEY = "x-device-uid";
+  static final String X_DEVICE_KEY_VALUE = "B84DEE8A08F9";
 }
