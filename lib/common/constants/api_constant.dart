54317891f54f965310d57df08b586d0e4270cc31
BaoNH19 <baonh19@fpt.com.vn>
2022-11-30 16:39:06 +0700

    upload structure

---
+++ lib/common/constants/api_constant.dart
@@ -0,0 +1,9 @@
+class ApiConstant {
+  static final String BASE_URL = "https://serverappsale.herokuapp.com/";
+  static final String SIGN_IN = "/user/sign-in";
+  static final String SIGN_UP = "/user/sign-in";
+  static final String PRODUCT = "/product";
+  static final String CART = "/cart";
+  static final String ADD_CART = "/cart/add";
+  static final String UPDATE_CART = "/cart/update";
+}
---
+++ lib/common/constants/variable_constant.dart
@@ -0,0 +1,8 @@
+class VariableConstant {
+  static final String TOKEN = "token";
+  static final String ROUTE_SIGNIN = "sign-in";
+  static final String ROUTE_SIGNUP =  "sign-up";
+  static final String ROUTE_HOME =  "home-page";
+  static final String ROUTE_CARTPAGE =  "cart-page";
+
+}
